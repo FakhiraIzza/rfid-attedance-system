ecbe00881950bb00723bcbabea07a4b998ba7a21
FakhiraIzza <fakhiraiffa@gmail.com>
2026-08-04 18:20:14 +0700

    Delete public_html (1)/database/migrations directory

--- public_html (1)/database/migrations/20260107_add_email_reset.sql
+++
@@ -1,7 +0,0 @@
--- Tambah kolom email + reset token untuk password orang tua
-ALTER TABLE users
-  ADD COLUMN email VARCHAR(150) NULL AFTER username,
-  ADD COLUMN reset_token_hash VARCHAR(64) NULL AFTER password_hash,
-  ADD COLUMN reset_token_expires DATETIME NULL AFTER reset_token_hash;
-
-CREATE UNIQUE INDEX uniq_users_email ON users (email);
