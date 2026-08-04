6a33c3c114c3d07abc7b3e1269b8d402c8335687
FakhiraIzza <fakhiraiffa@gmail.com>
2026-08-04 18:13:45 +0700

    Update db.php

--- public_html (1)/config/db.php
+++ public_html (1)/config/db.php
@@ -1,11 +1,9 @@
 <?php
-// =====================================================
-// Konfigurasi Database MySQL (XAMPP/phpMyAdmin)
-// =====================================================
+
 return [
-  'host' => 'localhost',
-  'db'   => 'u597718645_presensi',
-  'user' => 'u597718645_admin',
-  'pass' => 'PresensiSMPIA123$',
-  'charset' => 'utf8mb4',
+    'host' => 'localhost',
+    'db' => 'YOUR_DATABASE_NAME',
+    'user' => 'YOUR_DATABASE_USERNAME',
+    'pass' => 'YOUR_DATABASE_PASSWORD',
+    'charset' => 'utf8mb4',
 ];
